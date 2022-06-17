db902e75253d191d508be4608ddfc8324ff54e22
Andrew <cosminfuica@gmail.com>
2022-06-17 20:09:01 +0300

    Update Main.java

--- JavaFitnessRoutine/src/main/java/Main.java
+++ JavaFitnessRoutine/src/main/java/Main.java
@@ -7,8 +7,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduceti numele persoanei care se antreneaza");
         String name = scanner.nextLine();
+        //Cream un obiect de tip trainee
         Trainee trainee = new Trainee(name);
         System.out.println("Introduceti ziua curenta");
+        //Selectam antrenamentul in functie de input
         String day = scanner.nextLine();
         while ((day.equals("luni") || day.equals("miercuri") || day.equals("vineri")) && trainee.getStamina() > 0) {
             switch (day) {
@@ -30,12 +32,14 @@ public class Main {
             }
             day = scanner.nextLine();
         }
+        //Daca ziua introdusa nu se afla in switch vom afisa un mesaj
         if (!day.equals("luni") || !day.equals("miercuri") || !day.equals("vineri")) {
             System.out.println(day + " nu este zi de antrenament pentru " + trainee.getName());
         }
         if (trainee.getStamina() <= 0) {
             System.out.println(trainee.getName() + " este mult prea obosit. Nivelul energie este: " + trainee.getStamina());
         }
+        //Statusul trainee-ului
         System.out.println(trainee.getName() + "  Strength: " + trainee.getStrength() + "  Stamina: " + trainee.getStamina());
     }
 }
